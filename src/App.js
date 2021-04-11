93d944be78e544de5b8e54d0b715caa7d536867b
mrkentung <widiyanto.adhi@gmail.com>
2021-04-11 21:40:08 +0700

    refactor(redux): implement redux to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import './App.css';
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import * as actions from './redux/actions'
 
-function App() {
+import './App.css'
+import Table from '../src/components/Table'
+
+function App(props) {
+  useEffect(() => {
+    props.getFish()
+  });
+
+  const { fishData, addFish } = props
+
   return (
     <div className="App">
       <header className="bg-green-400">
@@ -9,64 +20,13 @@ function App() {
         </div>
       </header>
       <div className="max-w-screen-xl mx-auto py-16">
-        <div className="flex flex-col">
-          <div clasNames="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Komoditas
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Provinsi
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Kota
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Harga
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Tanggal 
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                  <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="flex items-center">
-                        <div class="flex-shrink-0 h-10 w-10">
-                          <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="" />
-                        </div>
-                        <div class="ml-4">
-                          <div class="text-sm font-medium text-gray-900">
-                            Jane Cooper
-                          </div>
-                          <div class="text-sm text-gray-500">
-                            jane.cooper@example.com
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                      <div class="text-sm text-gray-500">Optimization</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      Admin
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+        <div className="shadow-md p-4">
+          <div className="flex flex-col">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <Table 
+                data={fishData}
+                addFishData={addFish}
+              />
             </div>
           </div>
         </div>
@@ -75,4 +35,8 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  fishData: state.fishData
+})
+
+export default connect(mapStateToProps, actions)(App);
